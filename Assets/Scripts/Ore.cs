6fdc2f810324366c561c31338a28d3e0c418edca
Buzu Alexandru <mr.buzu00@gmail.com>
2023-05-17 20:28:25 +0300

    added pixelated fonts, basic ore functionality

--- Assets/Scripts/Ore.cs
+++ Assets/Scripts/Ore.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ore : MonoBehaviour
 {
+    public string type;
+    public float maxHP;
     public float hitPoints;
-    public Rigidbody2D rb;
+    public PolygonCollider2D pc;
+    public SpriteRenderer sr;
+    public Slider hpBar;
+    public float respawn = 60f;
     void Start()
     {
-        
+        hitPoints = maxHP;
+        hpBar.maxValue = maxHP;
+        hpBar.value = hitPoints;
     }
 
     void Update(){
@@ -16,10 +24,26 @@ public class Ore : MonoBehaviour
     }
 
     void OnTriggerEnter2D(Collider2D theCollision){
-        if (theCollision.CompareTag("Ladder"))
+        if (theCollision.CompareTag("Bullet"))
         {
             hitPoints -= 1f;
+            hpBar.value = hitPoints;
+            if(hitPoints == 0){
+                pc.enabled = false;
+                sr.enabled = false;
+                hpBar.gameObject.SetActive(false);
+                StartCoroutine(RespawnDelay());
+            }
         }
+    }
+
+    IEnumerator RespawnDelay(){
+        yield return new WaitForSeconds(respawn);
+        pc.enabled = true;
+        sr.enabled = true;
+        hpBar.gameObject.SetActive(true);
+        hitPoints = maxHP;
+        hpBar.value = hitPoints;
     }
 
 }
